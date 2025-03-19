4b92fa0a0a0360232f7af2a6936d10b62e7215ad
Aarongmez18 <aaron.gomez-delgado@iesruizgijon.com>
2025-03-19 22:58:39 +0100

    first commit and include banner.txt

---
+++ src/main/java/com/backend/automovilSevilla/AutomovilSevillaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.automovilSevilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomovilSevillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomovilSevillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/automovilSevilla/AutomovilSevillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.automovilSevilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomovilSevillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
